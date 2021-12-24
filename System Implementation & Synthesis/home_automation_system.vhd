8cfa19a7f59c5fb086d1eed032a4cc015f621bf3
Mostafa Elgendy <62804966+mostafa-elgendy22@users.noreply.github.com>
2021-12-24 18:56:20 +0200

    ignores

---
+++ System Implementation & Synthesis/home_automation_system.vhd
@@ -0,0 +1,14 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY home_automation_system IS
+       PORT (
+              clk : IN STD_LOGIC;
+              reset : IN STD_LOGIC;
+              SFD, SRD, SW, SFA, ST : IN STD_LOGIC;
+              --The temperature range from 9 to 40 degree celsius (can be represented with 5-bits "log2(40 - 9 + 1)")
+              temperature : IN STD_LOGIC_VECTOR(4 DOWNTO 0); 
+              front_door, rear_door, window_buzz, alarm_buzz, heater, cooler : OUT STD_LOGIC;
+              display : OUT STD_LOGIC_VECTOR(2 DOWNTO 0)
+       );
+END ENTITY;
