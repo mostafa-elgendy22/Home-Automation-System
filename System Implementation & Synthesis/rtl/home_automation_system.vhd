f5769af85be327e3001eedce719f1daca01aa413
Mostafa Elgendy <62804966+mostafa-elgendy22@users.noreply.github.com>
2021-12-24 22:19:52 +0200

    priority encoder

--- System Implementation & Synthesis/rtl/home_automation_system.vhd
+++ System Implementation & Synthesis/rtl/home_automation_system.vhd
@@ -6,8 +6,8 @@ ENTITY home_automation_system IS
               clk : IN STD_LOGIC;
               reset : IN STD_LOGIC;
               SFD, SRD, SW, SFA, ST : IN STD_LOGIC;
-              --The temperature range from 9 to 40 degree celsius (can be represented with 5-bits "log2(40 - 9 + 1)")
-              temperature : IN STD_LOGIC_VECTOR(4 DOWNTO 0); 
+              --The temperature range from 41 to 104 degree fahrenheit (5 to 40 degree celsius) (can be represented with 6-bits "floor(log2(104 - 41 + 1))")
+              temperature : IN STD_LOGIC_VECTOR(5 DOWNTO 0); 
               front_door, rear_door, window_buzzer, alarm_buzzer, heater, cooler : OUT STD_LOGIC;
               display : OUT STD_LOGIC_VECTOR(2 DOWNTO 0)
        );
---
+++ System Implementation & Synthesis/rtl/priority_encoder.vhd
@@ -0,0 +1,29 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY priority_encoder IS
+       PORT (
+              reset, SFD, SRD, SW, SFA, ST : IN STD_LOGIC;
+              temperature : IN STD_LOGIC_VECTOR(5 DOWNTO 0);
+              A : OUT STD_LOGIC_VECTOR(2 DOWNTO 0)
+       );
+END ENTITY;
+
+ARCHITECTURE priority_encoder OF priority_encoder IS
+BEGIN
+       A <= "000" WHEN reset = '1'
+              ELSE
+              "001" WHEN SFD = '1'
+              ELSE
+              "010" WHEN SRD = '1'
+              ELSE
+              "011" WHEN SFA = '1'
+              ELSE
+              "100" WHEN SW = '1'
+              ELSE
+              "101" WHEN (ST = '1') AND (temperature < "001001")      --temperature is less than 50F
+              ELSE
+              "110" WHEN (ST = '1') AND (temperature > "011101")      --temperature is greater than 70F
+              ELSE
+              "000";
+END ARCHITECTURE;
