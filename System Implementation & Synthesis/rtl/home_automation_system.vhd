9858b2c3e9940ab22575c5fc1836d37b2260d4e0
Mostafa Elgendy <62804966+mostafa-elgendy22@users.noreply.github.com>
2021-12-24 19:20:32 +0200

    Update home_automation_system.vhd

--- System Implementation & Synthesis/rtl/home_automation_system.vhd
+++ System Implementation & Synthesis/rtl/home_automation_system.vhd
@@ -8,7 +8,7 @@ ENTITY home_automation_system IS
               SFD, SRD, SW, SFA, ST : IN STD_LOGIC;
               --The temperature range from 9 to 40 degree celsius (can be represented with 5-bits "log2(40 - 9 + 1)")
               temperature : IN STD_LOGIC_VECTOR(4 DOWNTO 0); 
-              front_door, rear_door, window_buzz, alarm_buzz, heater, cooler : OUT STD_LOGIC;
+              front_door, rear_door, window_buzzer, alarm_buzzer, heater, cooler : OUT STD_LOGIC;
               display : OUT STD_LOGIC_VECTOR(2 DOWNTO 0)
        );
 END ENTITY;
