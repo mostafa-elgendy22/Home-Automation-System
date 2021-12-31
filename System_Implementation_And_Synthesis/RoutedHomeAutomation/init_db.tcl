4b904524ece92ea7d662906de81fb9bb7d6fecb5
Ahmed Ihab <57008633+ahmedihabb2@users.noreply.github.com>
2021-12-31 23:38:41 +0200

    Delete System_Implementation_And_Synthesis/RoutedHomeAutomation directory

--- System_Implementation_And_Synthesis/RoutedHomeAutomation/init_db.tcl
+++
@@ -1,7 +0,0 @@
-if {[catch {$info}]} {
-   set db_param [load_db [file dirname [file normalize [info script]]] -info $info]
-} else {
-   set db_param [load_db [file dirname [file normalize [info script]]]]
-}
-load_libs
-load_design -db $db_param
